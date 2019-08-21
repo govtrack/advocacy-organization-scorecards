fe07906eb40c5d403a97ff119766a7c24145e25d
Joshua Tauberer <tauberer@govtrack.us>
2019-08-21 20:34:06 +0000

    check that percent scores are integers

--- lint.py
+++ lint.py
@@ -40,6 +40,19 @@ for fn in sorted(glob.glob("scorecards/*.yaml")):
 		# Load the remaining data as a TSV.
 		scores = list(csv.reader(io.StringIO(scores), delimiter=","))
 
+		def is_int(v):
+			try:
+				int(v)
+			except ValueError:
+				return False
+			return True
+
+		# Validate data.
+		for row in scores:
+			if len(row) != 3: raise ValueError("Not three columns: " + repr(row) + " (" + fn + ")")
+			if row[1].strip() == "": raise ValueError("Empty score in " + repr(row) + " (" + fn + ")")
+			if metadata["type"] == "percent" and not is_int(row[1]): raise ValueError("Invalid score (must be an integer for percent scores) in " + repr(row) + " (" + fn + ")")
+
 		# Write it out again.
 		f.seek(0)
 		f.truncate()
@@ -47,13 +60,8 @@ for fn in sorted(glob.glob("scorecards/*.yaml")):
 		f.write("...\n")
 		w = csv.writer(f, delimiter=",", lineterminator="\n")
 		for row in scores:
-			# Validate data.
-			if len(row) != 3: raise ValueError("Not three columns: " + repr(row) + " (" + fn + ")")
-			if row[1].strip() == "": raise ValueError("Empty score in " + repr(row) + " (" + fn + ")")
-
 			# Left-pad the score column to make it easier to scan visually.
 			row[1] = " "*(3-len(row[1])) + row[1]
-
 			w.writerow(row)
 
 		print(metadata['name'])
